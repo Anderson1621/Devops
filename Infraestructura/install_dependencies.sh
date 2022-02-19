cd02013b49bf6cd4d60ae3bf01edecac94f348b4
Anderson Salguero <anderson.salguero@kibernum.com.co>
2022-02-19 18:27:28 -0500

    Versionamiento del codigo para prueba devops

--- Infraestructura/install_dependencies.sh
+++ Infraestructura/install_dependencies.sh
@@ -1,11 +1,6 @@
 #!/bin/bash 
 ls -al
 
-echo "******************************"
-echo "Provisioning Python Code"
-echo "******************************"
-
-# Loop that looks for the dpendencies file and installs them
 find . -name "dependencies.lst" -exec sh -c '
 for file do
  dir=${file%/*}
